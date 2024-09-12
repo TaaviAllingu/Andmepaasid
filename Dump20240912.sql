9ed22cb892e99285217573193519bcb47231ef5d
TaaviAllingu <147393230+TaaviAllingu@users.noreply.github.com>
2024-09-12 10:35:34 +0300

    Dump20240912.sql

--- Dump20240912.sql
+++ Dump20240912.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ekask
+-- Host: 127.0.0.1    Database: talllingu
 -- ------------------------------------------------------
 -- Server version	8.0.39
 
